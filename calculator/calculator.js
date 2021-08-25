25c8f3a19a82b352db99fa9a38b2852e7df3f52f
Mandy Woodbury <85962983+mixtape85@users.noreply.github.com>
2021-08-25 12:38:37 -0400

    add initial values and monthly payment calculation

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,6 +21,26 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+    
+  const initialValues = {
+    amount: document.getElementById("loan-amount").value = 250000,
+    years: document.getElementById("loan-years").value = 30,
+    rate: document.getElementById("loan-rate").value = 3.75
+    }
+  
+    document.getElementById("loan-amount").style.color = 'gray';
+    document.getElementById("loan-amount").style.fontStyle = 'italic';
+
+    document.getElementById("loan-years").style.color = 'gray';
+    document.getElementById("loan-years").style.fontStyle = 'italic';
+
+    document.getElementById("loan-rate").style.color = 'gray';
+    document.getElementById("loan-rate").style.fontStyle = 'italic';
+
+    document.getElementById("monthly-payment").style.color = 'gray';
+    document.getElementById("monthly-payment").style.fontStyle = 'italic';
+
+  calculateMonthlyPayment(initialValues);
 }
 
 // Get the current values from the UI
@@ -32,6 +52,15 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  console.log(`calc monthly payments function ran for ${values}`);
+  let P = values.amount;
+  let iRate = values.rate/100/12;
+  let num = values.years*12;
+
+  let result = (P*iRate)/(1-Math.pow((1+iRate),(-1*num)));
+  console.log(result.toFixed(2));
+  document.getElementById("monthly-payment")
+  document.getElementById("monthly-payment").innerText = `$ ${result.toFixed(2)}`;
 }
 
 // Given a string representing the monthly payment value,
