73c30523bf1b9a951c9b8955a6ab754808431987
Mandy Woodbury <85962983+mixtape85@users.noreply.github.com>
2021-08-28 00:34:19 -0400

    update all js functions, remove console.log msgs, add button hover effect

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,6 +1,6 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect().toEqual();
 });
 
 
@@ -9,3 +9,28 @@ it("should return a result with 2 decimal places", function() {
 });
 
 /// etc
+
+// it should throw an error if inputs are invalid - input of $ or , or letters
+// it should pull the values from the DOM
+// it should have initial values set up and calculate an inital monthly payment
+
+//setupInitialValues()
+//it should have initial values set up and calculate an initial monthly payment
+//it should change the color to gray
+
+//calculateMonthlyPayment
+// it should correctly calculate payment
+// it should return 2 decimals
+// it should throw an error if any value is blank
+// it should throw an error if a inputs are invalid (letters, symbols, etc)
+// it should throw an error if % isn't between 0-100%
+
+//updateMonthly
+//it should update the DOM with the new monthly payment
+//it should be two decimal places
+//it should be a string
+//it should have a comma separator, if applicable
+
+
+//update()
+//it should update user input values and recalculate monthly payment
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -23,22 +23,12 @@ function getCurrentUIValues() {
 function setupIntialValues() {
     
   const initialValues = {
+    //Currency format (may not use - keep for future use): 
+    //Intl.NumberFormat('en-US',{ style: 'currency', currency: 'USD' }).format(250000),
     amount: document.getElementById("loan-amount").value = 250000,
     years: document.getElementById("loan-years").value = 30,
     rate: document.getElementById("loan-rate").value = 3.75
     }
-  
-    document.getElementById("loan-amount").style.color = 'gray';
-    document.getElementById("loan-amount").style.fontStyle = 'italic';
-
-    document.getElementById("loan-years").style.color = 'gray';
-    document.getElementById("loan-years").style.fontStyle = 'italic';
-
-    document.getElementById("loan-rate").style.color = 'gray';
-    document.getElementById("loan-rate").style.fontStyle = 'italic';
-
-    document.getElementById("monthly-payment").style.color = 'gray';
-    document.getElementById("monthly-payment").style.fontStyle = 'italic';
 
   calculateMonthlyPayment(initialValues);
 }
@@ -46,23 +36,41 @@ function setupIntialValues() {
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const inputValues = {
+    amount: document.getElementById("loan-amount").value,
+    years: document.getElementById("loan-years").value,
+    rate: document.getElementById("loan-rate").value
+    }
+
+  calculateMonthlyPayment(inputValues);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  console.log(`calc monthly payments function ran for ${values}`);
   let P = values.amount;
   let iRate = values.rate/100/12;
   let num = values.years*12;
+  let result = ((P*iRate)/(1-Math.pow((1+iRate),(-1*num)))).toFixed(2);
 
-  let result = (P*iRate)/(1-Math.pow((1+iRate),(-1*num)));
-  
-  document.getElementById("monthly-payment").innerText = `$ ${result.toFixed(2)}`;
+  if(values.amount === '' || values.rate === '' || values.years === ''){
+    alert('Blank input! Please provide values for all inputs.');
+  }
+
+  if(values.rate <=0 || values.rate >100){
+    alert('Invalid input! Yearly rate must be between 0% and 100%.');
+  } else{
+    if(isNaN(result)){
+      alert('Invalid input! Use only numbers and decimals!');
+    }
+  }
+
+  updateMonthly(result);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {
+function updateMonthly(monthly) {  
+  document.getElementById("monthly-payment").innerText = `$ ${parseFloat(monthly).toLocaleString()}`;
 }
